f322e667f081ba8a3a835f24d54073abdd80ba5d
LaGereFou <36414571+LaGereFou@users.noreply.github.com>
2019-06-09 13:56:35 +0200

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,102 @@
+package com.alex.sms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Grade {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	@NotNull
+	@ManyToOne
+	private ReportCardDetail reportCardDetail;
+
+	@NotNull
+	double value;
+
+	@NotNull
+	String appreciation;
+
+	/**
+	 * 
+	 */
+	public Grade() {
+		super();
+	}
+
+	/**
+	 * @param id
+	 * @param reportCardDetail
+	 * @param value
+	 * @param appreciation
+	 */
+	public Grade(Integer id, ReportCardDetail reportCardDetail, double value, String appreciation) {
+		super();
+		this.id = id;
+		this.reportCardDetail = reportCardDetail;
+		this.value = value;
+		this.appreciation = appreciation;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the reportCardDetail
+	 */
+	public ReportCardDetail getReportCardDetail() {
+		return reportCardDetail;
+	}
+
+	/**
+	 * @param reportCardDetail the reportCardDetail to set
+	 */
+	public void setReportCardDetail(ReportCardDetail reportCardDetail) {
+		this.reportCardDetail = reportCardDetail;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public double getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the appreciation
+	 */
+	public String getAppreciation() {
+		return appreciation;
+	}
+
+	/**
+	 * @param appreciation the appreciation to set
+	 */
+	public void setAppreciation(String appreciation) {
+		this.appreciation = appreciation;
+	}
+}
